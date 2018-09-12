9ad72094c87996208be1a373c216cee1f9c77268
Amil Masic <amil.masic@gmail.com>
2018-09-12 17:14:55 +0000

    automatically backed up by learn

--- prime.rb
+++ prime.rb
@@ -4,9 +4,9 @@ def prime?(numbers)
   numbers = (-120..120).to_a
   numbers.any? do |prime|
     # if prime % 2 == 0 && prime % 3 == 0 && prime % 5 == 0 && prime % 7 == 0
-    if prime <= -1
-      false
-    elsif prime == 2 || prime == 3 || prime == 5 || prime == 7
+    # if prime <= -1
+    #   false
+    if prime == 2 || prime == 3 || prime == 5 || prime == 7
         true
       # elsif prime <= 0
       #   false
