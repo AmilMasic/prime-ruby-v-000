9434002ddfc94992e782c2269dcb1a70addffe34
Amil Masic <amil.masic@gmail.com>
2018-09-12 17:56:30 +0000

    automatically backed up by learn

--- prime.rb
+++ prime.rb
@@ -7,7 +7,7 @@ def prime?(numbers)
 
     if prime == 2 || prime == 3 || prime == 5 || prime == 7
         true
-      elsif prime < 0
+      elsif prime.between?(0..numbers)
         false
       else prime % 2 != 0 || prime % 3 != 0 || prime % 5 != 0 || prime % 7 != 0 ||
         false
