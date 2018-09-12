5590fdd1ade38053da934cf2f83e40e75a1e89b1
Amil Masic <amil.masic@gmail.com>
2018-09-12 17:10:42 +0000

    automatically backed up by learn

--- prime.rb
+++ prime.rb
@@ -1,15 +1,15 @@
 # Add  code here!
 require "pry"
 def prime?(numbers)
-  numbers = (0..120).to_a
+  numbers = (-120..120).to_a
   numbers.any? do |prime|
     # if prime % 2 == 0 && prime % 3 == 0 && prime % 5 == 0 && prime % 7 == 0
       if prime == 2 || prime == 3 || prime == 5 || prime == 7
         true
-      elsif prime % 2 != 0 || prime % 3 != 0 || prime % 5 != 0 || prime % 7 != 0
+      elsif prime % 2 != 0 || prime % 3 != 0 || prime % 5 != 0 || prime % 7 != 0 || prime < 0
         false
-      else
-        false
+    
+          
       end
   end
 end
