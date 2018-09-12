f929688a95e550cfbaa7391bf5d2bea4e506bf93
Amil Masic <amil.masic@gmail.com>
2018-09-12 16:46:52 +0000

    automatically backed up by learn

--- prime.rb
+++ prime.rb
@@ -3,4 +3,15 @@ require "pry"
 def prime?(numbers)
   # numbers = (1..120).to_a
   # binding.pry
+  numbers.each do |prime|
+    if prime % 2 == 0
+      false
+    elsif prime % 3 == 0
+      false
+    elsif prime % 5 == 0
+      false
+    elsif prime % 7 == 0
+      false
+    else
+      true
 end
