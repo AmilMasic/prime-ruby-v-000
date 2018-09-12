5b4cff60ccfc9af8ba0f2f01b3c2e4b94486ba2c
Amil Masic <amil.masic@gmail.com>
2018-09-12 16:57:51 +0000

    automatically backed up by learn

--- prime.rb
+++ prime.rb
@@ -1,13 +1,6 @@
 # Add  code here!
 require "pry"
-# def prime?(numbers)
-#   i = 0
-#   while i < numbers.size
-#     if numbers[i] % 2 == 0
-#       puts "be sure to account for negative numbers"
-#     else
-#       true
-#     end
-#     i +=1
-#   end
-# end
+def prime?(numbers)
+ numbers = []
+
+end
