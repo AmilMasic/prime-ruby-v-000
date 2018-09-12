eb8d95dbab6eea404b154b1a894aad814b85b267
Amil Masic <amil.masic@gmail.com>
2018-09-12 17:08:15 +0000

    automatically backed up by learn

--- prime.rb
+++ prime.rb
@@ -2,7 +2,7 @@
 require "pry"
 def prime?(numbers)
   numbers = (0..120).to_a
-  numbers.all? do |prime|
+  numbers.any? do |prime|
     # if prime % 2 == 0 && prime % 3 == 0 && prime % 5 == 0 && prime % 7 == 0
       if prime == 2 || prime == 3 || prime == 5 || prime == 7
         true
