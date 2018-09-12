c68209924e1e933aa0fb9a6993112af2f5465e3a
Amil Masic <amil.masic@gmail.com>
2018-09-12 16:53:08 +0000

    automatically backed up by learn

--- prime.rb
+++ prime.rb
@@ -3,23 +3,23 @@ require "pry"
 def prime?(numbers)
   # numbers = (1..120).to_a
   # binding.pry
-  numbers.detect do |prime|
+  numbers.include do |prime|
     if prime % 2 == 0
       # return false
       puts "Be sure to account for negative numbers!"
-
+  
     elsif prime % 3 == 0
       # return false
        puts "Be sure to account for negative numbers!"
-
+  
     elsif prime % 5 == 0
       # return false
       puts "Be sure to account for negative numbers!"
-
+  
     elsif prime % 7 == 0
       # return false
       puts "Be sure to account for negative numbers!"
-
+  
     else
       # puts "Be sure to account for negative numbers!"
       true
