6cc0b1297bfba7ff83a0ee71496c030b6b3a39f3
Amil Masic <amil.masic@gmail.com>
2018-09-12 17:19:58 +0000

    automatically backed up by learn

--- prime.rb
+++ prime.rb
@@ -9,8 +9,8 @@ def prime?(numbers)
     #   false
     if prime == 2 || prime == 3 || prime == 5 || prime == 7
         true
-      # elsif prime <= 0
-      #   false
+      elsif prime <= 0
+        false
       else prime % 2 != 0 || prime % 3 != 0 || prime % 5 != 0 || prime % 7 != 0 ||
         false
 
