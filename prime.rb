72a09fa60fd10945f81c76fc523106034e73ef34
Amil Masic <amil.masic@gmail.com>
2018-09-12 17:58:14 +0000

    automatically backed up by learn

--- prime.rb
+++ prime.rb
@@ -2,9 +2,9 @@
 require "pry"
 def prime?(numbers)
   # numbers = (-120..120).to_a
-  # numbers = numbers.to_a
-  array = array.between?(2..numbers)
-  array.any? do |prime|
+  numbers = numbers.to_a
+  # array = array.between?(2..numbers)
+  numbers.any? do |prime|
 
     if prime == 2 || prime == 3 || prime == 5 || prime == 7
         true
